8ea25f39b5c0a8bf1e1f9661bc5267eef453b2c5
sanjaymadaan <95046505+sanjaymadaan@users.noreply.github.com>
2025-01-21 09:44:54 +0530

    Update PascalTriangle.java

--- pack1/PascalTriangle.java
+++ pack1/PascalTriangle.java
@@ -1,11 +1,26 @@
-package pack1;
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+         if(numRows == 0)
+         return new ArrayList<>();
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+         if(numRows == 1){
+                     List<List<Integer>> result = new ArrayList<>();
+                     result.add(Arrays.asList(1));
+                     return result;
+         }
 
-public class PascalTriangle {
-		
-    public static void main(String[] args) {
-	}
+         List<List<Integer>> prevRows = generate(numRows - 1);
+
+         List<Integer> newRow = new ArrayList<>();
+        
+        for(int i =0; i< numRows; i++){
+            newRow.add(1);
+        }
+        for (int i = 1; i < numRows - 1; i++) {
+            newRow.set(i, prevRows.get(numRows - 2).get(i - 1) + prevRows.get(numRows - 2).get(i));
+        }
+        
+        prevRows.add(newRow);
+        return prevRows;
+    }
 }
