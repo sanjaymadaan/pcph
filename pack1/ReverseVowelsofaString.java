d062a7288b2b246c09a306765a28d755cc4411d1
sanjaymadaan <95046505+sanjaymadaan@users.noreply.github.com>
2025-01-30 09:04:25 +0530

    Update ReverseVowelsofaString.java

--- pack1/ReverseVowelsofaString.java
+++ pack1/ReverseVowelsofaString.java
@@ -1,12 +1,32 @@
 class ReverseVowelsofaString {
-    public void reverseString(char[] s) {
-     	int left = 0;
-        int right = s.length - 1;
-        while(left < right){
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++; right--; 
-        }
-    }
+	public static boolean isVowel(char ch) {
+		List<Character> list = Arrays.asList('a','e','i','o','u'
+				,'A','E','I','O','U');
+		return list.contains(ch);
+	}
+
+    public String reverseVowels(String s) {
+      char arr[] = s.toCharArray();
+	int left = 0;
+	int right = arr.length - 1;
+	while(left < right) {
+		if(isVowel(arr[left]) && isVowel(arr[right])) {
+			char temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			left++; right--;
+		} 
+		if(!isVowel(arr[left])) {
+			left++;
+		}
+		if(!isVowel(arr[right])) {
+			right--;
+		}	
+	}
+	String reqstr = new String("");
+	for(char ch : arr) {
+		reqstr = reqstr.concat(""+ch);
+	}
+	return reqstr;  
+   }
 }
